fd6263b0a65f8be72be3e1bcfe4bfa329f06af63
crisncris0000 <christopherrivera384@gmail.com>
2023-07-06 15:40:50 -0400

    Added backend and frontend currently working on the frontend

---
+++ crafts-be/src/main/java/com/crafts/craftsbe/CraftsBeApplication.java
@@ -0,0 +1,13 @@
+package com.crafts.craftsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftsBeApplication.class, args);
+	}
+
+}
---
+++ crafts-be/src/test/java/com/crafts/craftsbe/CraftsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crafts.craftsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
