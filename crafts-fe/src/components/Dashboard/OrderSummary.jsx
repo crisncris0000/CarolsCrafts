ccd9e11704ee9bc76a5ddd6339eb012d7ef3b010
crisncris0000 <christopherrivera384@gmail.com>
2023-07-11 11:02:56 -0400

    UI changes to dashboard

--- crafts-fe/src/components/Dashboard/OrderSummary.jsx
+++ crafts-fe/src/components/Dashboard/OrderSummary.jsx
@@ -3,24 +3,24 @@ import { CButton, CModal, CModalHeader, CModalTitle, CModalBody, CModalFooter } 
 
 export default function OrderSummary() {
     const [visible, setVisible] = useState(false)
-  return (
-    <>
-        <CButton onClick={() => setVisible(!visible)} className= 'modal-button'>Summary</CButton>
-        <CModal alignment="center" visible={visible} onClose={() => setVisible(false)} style>
-            <CModalHeader>
-                <CModalTitle>Summary of Transactions</CModalTitle>
-            </CModalHeader>
-            <CModalBody>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-                egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-            </CModalBody>
-            <CModalFooter>
-                <CButton color="secondary" onClick={() => setVisible(false)}>
-                Close
-                </CButton>
-                <CButton color="primary">Save changes</CButton>
-            </CModalFooter>
-        </CModal>
-    </>
+    return (
+        <>
+            <CButton onClick={() => setVisible(!visible)} className= 'modal-button'>Summary</CButton>
+            <CModal alignment="center" visible={visible} onClose={() => setVisible(false)}>
+                <CModalHeader>
+                    <CModalTitle>Summary of Transactions</CModalTitle>
+                </CModalHeader>
+                <CModalBody>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
+                    egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                </CModalBody>
+                <CModalFooter>
+                    <CButton color="secondary" onClick={() => setVisible(false)}>
+                    Close
+                    </CButton>
+                    <CButton color="primary">Save changes</CButton>
+                </CModalFooter>
+            </CModal>
+        </>
   )
 }
