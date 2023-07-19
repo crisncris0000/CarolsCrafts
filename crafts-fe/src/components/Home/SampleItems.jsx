866805ad7d7fcab5a82f93a80bdbeed7355d52a0
crisncris0000 <christopherrivera384@gmail.com>
2023-07-19 19:23:56 -0400

    added button

--- crafts-fe/src/components/Home/SampleItems.jsx
+++ crafts-fe/src/components/Home/SampleItems.jsx
@@ -5,6 +5,7 @@ import TurkeyImg from '../../images/carol-turkey.png';
 import Hats from '../../images/hats.png';
 import SantaCam from '../../images/christmas.png';
 import CustomSweater from '../../images/custom-sweater.png';
+import { Link } from 'react-router-dom';
 
 export default function SampleItems() {
   return (
@@ -19,7 +20,9 @@ export default function SampleItems() {
           <img src={Hats} alt="Manaquin wearing hat"/>
           <img src={CustomSweater} alt="Red sweater with attachments to it"/>
           <img src={SantaCam} alt="Santa Cam"/>
-
+        </div>
+        <div className="btn-container">
+          <Link to={"/portfolio"}><button>See Work</button></Link>
         </div>
     </>
   )
