41fe3504fcc7f6746cf35c3b8eea87f95c409581
crisncris0000 <christopherrivera384@gmail.com>
2023-07-23 20:29:15 -0400

    merge

--- crafts-be/src/main/java/com/crafts/craftsbe/config/SecurityConfig.java
+++ crafts-be/src/main/java/com/crafts/craftsbe/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.crafts.craftsbe.config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,15 +20,18 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws  Exception{
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         http
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((auth) -> {
                     auth.requestMatchers("/api/create-user").permitAll();
                 }).csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((auth) -> {
-                    auth.requestMatchers("/users").permitAll();
+                    auth.requestMatchers("/api/users").permitAll();
                 });
 
         return http.build();
@@ -42,6 +49,15 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        
+        return authProvider;
     }
 
 }
