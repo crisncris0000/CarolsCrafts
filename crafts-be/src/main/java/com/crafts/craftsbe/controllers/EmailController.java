98f5ccd3ba05015a090c1522114fc7729b6ce477
crisncris0000 <christopherrivera384@gmail.com>
2023-10-29 12:29:44 -0400

    Adjusted the css for shoppiung card

--- crafts-be/src/main/java/com/crafts/craftsbe/controllers/EmailController.java
+++ crafts-be/src/main/java/com/crafts/craftsbe/controllers/EmailController.java
@@ -2,6 +2,7 @@ package com.crafts.craftsbe.controllers;
 
 import com.crafts.craftsbe.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ public class EmailController {
     @Autowired
     EmailService emailService;
 
+
     @PostMapping("/send-email")
     public ResponseEntity<String> sendEmail(@RequestBody Map<String, String> body) {
 
@@ -24,7 +26,9 @@ public class EmailController {
         String lastName = body.get("lastName");
         String emailBody = body.get("body");
 
-        emailService.sendEmail(email, "Email sent from " + firstName + " " + lastName, emailBody);
+        emailService.sendEmail("mckcar411@gmail.com", "Email sent from " + firstName + " " + lastName,
+                emailBody +
+                "\n\n You can reach them at this email: " + email);
 
         return new ResponseEntity<>("Email sent successfully", HttpStatus.OK);
     }
